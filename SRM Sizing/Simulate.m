418dca63cdf019903e709a3559f23ecc89cc3959
Blake Lowe <9019960+dragonfyre23@users.noreply.github.com>
2021-03-31 19:21:50 -0400

    Changes
    fixed A_t prediction to give correct max pressure.
    troubleshot thrust calculation

--- SRM Sizing/Simulate.m
+++ SRM Sizing/Simulate.m
@@ -1,7 +1,7 @@
 %% solid rocket motor sizing code
-function [T, W, P_c, Thrust, R_b, burn_time, Mdot, A_t, deltaV, specificImpulse, propMass] = Simulate(dt, shape, length, width, innerWidth, maxPres, f_inert)
+function [T, W, P_c, Thrust, R_b, burn_time, M, Mdot, A_t, deltaV, specificImpulse, propMass] = Simulate(dt, shape, length, width, innerWidth, maxPres, f_inert)
 %% Constants (SI)
-g = 9.81; 
+g = 9.80665; 
 %% Inputs
 %time step [seconds]
 %rocket geometry parameters: Width, inner width, shape, and length [all in meters]
@@ -14,10 +14,14 @@ C_star = 1077.8; %C (m/s)* for 70% AP-HTPB
 propDens = 1500; %Average density for 70% AP-HTPB (kg/m^3)
 c_t = 0.5; %units? who needs those
 
+if (width <= innerWidth)
+   error("Width is less than or equal to inner width. Impossible!");
+end
+
     r_max = width / 2;
     r_min = innerWidth / 2;
     %finding throat area for end of burn
-    A_t = ((Surface_Area(shape, r_min, length) * propDens * C * C_star) / (g * maxPres^(1-n)));
+    A_t = ((Surface_Area(shape, r_max, length) * propDens * C * C_star) / (g * maxPres^(1-n)));
     throatDiameter = 2*sqrt(A_t/pi); %m
     %A_t = 10;
     %set initial values
@@ -38,7 +42,7 @@ c_t = 0.5; %units? who needs those
         P_c = [P_c, 0];
         P_c(i) = ((Surface_Area(shape, W(i-1), length) * propDens * C * C_star) / (g * A_t))^(1/ 1 - n);
         %Thrust
-        Thrust(i) = c_t * A_t * P_c(i); %N
+        Thrust(i) = c_t * A_t * P_c(i); %N %c_t changes over time! this eqn doesn't apply %T=mdot*ve (this assumes ideal nozzle design)
         %Burn Rate
         R_b(i) = C * P_c(i)^n;
         %New web distance
@@ -51,8 +55,12 @@ c_t = 0.5; %units? who needs those
         %increment index
         i = i + 1;
     end
-    burn_time = T(i-1);
+    burn_time = T(end);
+    accel = Thrust(2:end)./M(2:end);
     deltaV = trapz(dt, Thrust(2:end)./M(2:end));
+    
+    v_e = 2000;
+    deltaVcheck = v_e*log(M(1)-M(end)); %need v_e
     specificImpulse = trapz(dt,Thrust)/(propMass*g);
 end
 
