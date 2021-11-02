7955cc3ccb3f154036fbb92e0a59859f3a701821
Blake Lowe <9019960+dragonfyre23@users.noreply.github.com>
2021-11-01 20:09:48 -0400

    simplified liquid_sizing

--- Rocket Sizing/sizing_liquid/liquid_sizing.m
+++ Rocket Sizing/sizing_liquid/liquid_sizing.m
@@ -1,4 +1,4 @@
-function [mass, volume] = liquid_sizing(isp, propMassFraction, payloadMass, deltaV, finenessRatio, propDensity, containerThickness);
+function [mass, volume] = liquid_sizing(isp, propMassFraction, payloadMass, deltaV, propDensity);
     %mass calculations
     massRatio = exp(deltaV/(g*isp));
     propMass = payloadMass * ((massRatio - 1) / (((1 - massRatio) / propMassFraction) + massRatio)); %[kg]
@@ -7,8 +7,4 @@ function [mass, volume] = liquid_sizing(isp, propMassFraction, payloadMass, delt
 
     %dimensioning
     propVolume = totalMass / propDensity; %[m^3]
-    diameter = (4 * propVolume / (pi * finenessRatio))^(1/3); %[m]
-    length = diameter * finenessRatio; %[m]
-    diameter = diameter + 2 * containerThickness;
-    crossSectionalArea = (1/4) * pi * (diameter^2); %[m^2]
 end
