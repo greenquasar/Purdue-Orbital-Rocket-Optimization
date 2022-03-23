4bf01fe657833224ea12d0a1328e4e050ae6e957
WildFunctionality <53576639+WildFunctionality@users.noreply.github.com>
2022-03-23 17:36:12 -0400

    beginnings of the excel file

--- SRM Sizing/WritetoExcel.m
+++ SRM Sizing/WritetoExcel.m
@@ -3,18 +3,13 @@ function [] = WritetoExcel(fuel,ox,fuel_oxRatio,length,width,innerwidth,totmass,
 % 
 % This is poorly written and needs to be fixed but it should work
 % MAKE SURE YOU HAVE EXCEL ON YOURE SYSTEM
+% thows an error if you have Output.xlsx open on your system
+
 filename = 'Output.xlsx';
-xlswrite(filename, length, 1, 'D2');
-xlswrite(filename, width, 1, 'D3');
-xlswrite(filename, innerwidth, 1, 'D4');
-xlswrite(filename, totmass, 1, 'D5');
-xlswrite(filename, mass1st, 1, 'D6');
-xlswrite(filename, mass2nd, 1, 'D7');
-xlswrite(filename, dV, 1, 'D8');
-xlswrite(filename, isp, 1, 'D9');
-xlswrite(filename, fuel, 1, 'D10');
-xlswrite(filename, ox, 1, 'D11');
-xlswrite(filename, fuel_oxRatio, 1, 'D12');
-xlswrite(filename, thrust_time, 2, '')
+
+scalars = [length, width, innerwidth, totmass, mass1st, mass2nd, dV, isp, fuel, ox, fuel_oxRatio];
+
+xlswrite(filename, scalars, 1, 'D2:D12');
+%xlswrite(filename, thrust_time, 2, '')
 end
 
