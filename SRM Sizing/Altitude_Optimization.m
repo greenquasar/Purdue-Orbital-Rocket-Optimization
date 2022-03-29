c5839f6b24c1fe090914350e9f326febbb68e72a
dragonfyre23 <9019960+dragonfyre23@users.noreply.github.com>
2022-03-28 20:20:06 -0500

    altitude optimization implemented

--- SRM Sizing/Altitude_Optimization.m
+++ SRM Sizing/Altitude_Optimization.m
@@ -1,16 +1,19 @@
 %optimization for single stage sounding rocket
-function [] = Altitude_Optimization(target_altitude, starting_altitude, TWRmin, TWRmax, ...
-    dt, maxPres, shape, f_inert, atmoPressure, OF, ...
+function [T, W, P_c, Thrust, TWR, R_b, burn_time, M, Mdot, A_t, deltaV, avgSpecificImpulse, propMass, C_t, C_star, Altitude, Drag, Velocity, final_altitude] = ...
+    Altitude_Optimization(target_altitude, starting_altitude, TWRmin, TWRmax, tolerance, ...
+    dt, maxPres, shape, f_inert, payloadMass, atmoPressure, OF, ...
     fuels, f_temps, f_densities, f_fracs, oxidizers, o_temps, o_densities, o_fracs)
-%%inputs
+%% Inputs
     %target_altitude: target altitude (m)
     %starting_altitude: Starting altitude (m)
     %TWRmin: minimum Thrust to Weight Ratio
     %TWRmax: maximum Thrust to Weight Ratio
+    %tolerance: acceptable error in TWR and altitude (fraction)
     %dt: time step (s)
     %maxPres: maximum chamber pressure stage can withstand (Pa)
     %shape: circular/square shape: "circular"/"square"
     %f_inert: inert mass fraction
+    %payloadMass: mass of payload (kg)
     %atmoPressure: atmospheric pressure (Pa)
     %OF: oxidizer to fuel ratio
     %fuels: CEA fuel names, string (can be a list must use double quotes)
@@ -22,7 +25,7 @@ function [] = Altitude_Optimization(target_altitude, starting_altitude, TWRmin, 
     %o_densities: fuel densities in solid state (kg/m^3) (same length as oxidizer)
     %o_fracs: mass fractions for each oxidizer (same length as oxidizer, sum to 1)
     
-%%outputs
+%% Outputs
     %T, array of time steps (s)
     %W, array of web distance (m)
     %P_c, array of chamber pressure (Pa)
@@ -42,4 +45,48 @@ function [] = Altitude_Optimization(target_altitude, starting_altitude, TWRmin, 
     %Drag, array drag forces (N)
     %Velocity, array of velocities (m/s)
 
+    
+%% Constants
+
+
+
+%% Program
+
+    %passthrough_args = [maxPres, shape, f_inert, payloadMass, atmoPressure, OF, fuels, f_temps, f_densities, f_fracs, oxidizers, o_temps, o_densities, o_fracs];
+    
+    %guesses
+    stage_length = 1;
+    stage_width = 0.1;
+    innerWidth = 0.05;
+    
+    %do while loop
+    while (1) 
+        [T, W, P_c, Thrust, TWR, R_b, burn_time, M, Mdot, A_t, deltaV, avgSpecificImpulse, propMass, C_t, C_star] = ...
+    Simulate_Reverse(dt, stage_length, stage_width, innerWidth, maxPres, shape, f_inert, payloadMass, atmoPressure, ...
+    OF, fuels, f_temps, f_densities, f_fracs, oxidizers, o_temps, o_densities, o_fracs);
+    [Altitude, Drag, Velocity] = altitude_analysis(Thrust, M, dt, stage_width, starting_altitude);
+        final_altitude = max(Altitude);
+        if (tolerated(final_altitude, target_altitude, tolerance)) %altitude
+            %maybe need to calculate how much to change the width or the
+            %length to keep volume const
+            %stage width
+            factor = target_altitude/final_altitude;
+            stage_width = stage_width * factor;
+        elseif (tolerated(max(TWR), TWRmax, tolerance)) %max TWR
+            %stage length
+            factor = TWRmax/max(TWR);
+            stage_length = stage_length * factor;
+        elseif (tolerated(TWR(1), TWRmin, tolerance)) %min TWR
+            %inner width
+            factor = TWRmin/TWR(1);
+            inner_width = inner_width * factor;
+        else
+            break;
+        end
+    end
+    
+end
+
+function ans = tolerated(value, target, tolerance)
+    ans = abs(target-value)/target < tolerance;
 end
